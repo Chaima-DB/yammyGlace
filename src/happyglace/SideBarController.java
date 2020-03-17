a97b9490d570f37ce6e79e23d54e6fdb2f3c1fce
Chaima-DB <chimox751@gmail.com>
2020-03-17 20:02:38 +0100

    hello

---
+++ src/happyglace/SideBarController.java
@@ -0,0 +1,20 @@
+package happyglace;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author HP
+ */
+public class SideBarController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
